3d36b595a2a1b566680a9ec4d53b4f7e33c3fbad
Bekzod Imomov <magiccodeuz@gmail.com>
2026-04-27 17:49:32 +0500

    Board added

--- checkers.py
+++ checkers.py
@@ -1,25 +1,36 @@
 from tkinter import *
 
 root = Tk(className='Checkers_TJA', useTk=1)
+root.attributes('-fullscreen', True)
 label = Label(root, text="Checkers coming...!")
 label.pack()
 
-C = Canvas(root, bg="yellow",
-           height=250, width=300)
+base_size = 320
 
-line = C.create_line(108, 120,
-                     320, 40,
-                     fill="green")
+game_height = 3*base_size
+game_width = 4*base_size
 
-arc = C.create_arc(180, 150, 80,
-                   210, start=0,
-                   extent=220,
-                   fill="red")
+C = Canvas(root, bg="#9FAA9F",
+           height=game_height, width=game_width)
 
-oval = C.create_oval(80, 30, 140,
-                     150,
-                     fill="blue")
+rectangle = C.create_rectangle(0.75*base_size, 0.25*base_size, 3.25*base_size,
+                   2.75*base_size, fill="#DDDDAA")
+
+
+def create_row(y,fill_check=1): 
+    i = 0
+    while i < 2.5:
+        fill_check += 1
+        cells = C.create_rectangle((0.75+i)*base_size, (y)*base_size, (1.0625+i)*base_size,
+                        (y+0.3125)*base_size,  fill="#BD9168" if fill_check%2 == 0 else "#DDDDAA")
+        i += 0.3125
+
+y = 0.25
+fill_check = 1
+while y < 2.5:
+    create_row(y,fill_check)
+    y +=0.3125
+    fill_check += 1
 
 C.pack()
-
 root.mainloop()
